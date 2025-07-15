212367084f00deb471bbc68db78f80bab8e28d36
bokyeong <wntjdals0412@gmail.com>
2025-07-15 23:49:03 +0900

    feat: 비율 수정

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: '0.0.0.0',
     port: 3000,
   },
   plugins: [
@@ -19,4 +19,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: '/',
 }));
