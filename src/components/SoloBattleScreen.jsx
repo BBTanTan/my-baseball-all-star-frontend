7f9dd7439e9db56d38366b319c9d1f6d9afc4815
bokyeong <wntjdals0412@gmail.com>
2025-07-20 19:26:14 +0900

    design: 여백 수정

--- src/components/SoloBattleScreen.jsx
+++ src/components/SoloBattleScreen.jsx
@@ -246,7 +246,7 @@ const SoloBattleScreen = ({ onBack }) => {
   }
   // step이 setup이 아닐 때는 기존 로직대로 화면 전환
   return (
-    <div className="min-h-screen p-4">
+    <div className="min-h-screen">
       <div className="max-w-4xl mx-auto">
         {step === 'team1-select' && (
           <TeamSelectionScreen 
